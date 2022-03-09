257306dad9705a5c50718be5b3ef72ac7c5f9931
LionyxML <rahul.juliato@gmail.com>
2022-03-08 22:02:14 -0300

    feat: adds bad code for frontmacs show on flycheck

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 const App: React.FC = () => {
-  return <>Blank Project</>;
+  const [message, setMessage] = useState("type a message on the input above");
+
+  const handleChange = (e: any): void => {
+    setMessage(e.target.value);
+  };
+
+  const handleFocus = (
+    e: React.FocusEventHandler<HTMLInputElement> | undefined
+  ): void => {
+    console.log("focused!", e.target.value);
+  };
+
+  return (
+    <>
+      <p>Below, an input.</p>
+      <input onChange={handleChange} onFocus={handleFocus} />
+      <p> That prints: {message}</p>
+    </>
+  );
 };
 
 export default App;
